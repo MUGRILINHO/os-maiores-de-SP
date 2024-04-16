2ccde0bf105f6a5005cd6a5c545b73e7dbeac534
MUGRILINHO <127216750+MUGRILINHO@users.noreply.github.com>
2024-04-16 07:55:13 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const botoes - document.querySelectorA11(".botao");
 
 for(let i=0;i <botoes.lenght;i++){
-    
-    console.log(botoes[i]);
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.lenght;j++){
+            botoes[j].classList.remove("ativo")
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+    console.log();
 }
