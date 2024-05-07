64a8e9afa4d1f3a42a5de5d077a750004967b59a
MUGRILINHO <127216750+MUGRILINHO@users.noreply.github.com>
2024-05-07 07:48:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,9 +6,16 @@ for(let i=0;i <botoes.lenght;i++){
 
         for(let j=0;j<botoes.lenght;j++){
             botoes[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo")
         }
 
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date ();
+
+contadores[0].textContent = tempoObjetivo1;
