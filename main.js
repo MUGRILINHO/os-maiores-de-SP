5d065291913c4ee65cd5e0f6081a4627c39694b9
MUGRILINHO <127216750+MUGRILINHO@users.noreply.github.com>
2024-05-09 10:47:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorA11(".botao");
 const botoes = document.querySelectorA11(".aba-conteudo");
 
-for(let i=0;i <botoes.lenght;i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.lenght; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j<botoes.lenght;j++){
+        for (let j = 0; j < botoes.lenght; j++) {
             botoes[j].classList.remove("ativo")
             textos[j].classList.remove("ativo")
         }
@@ -19,8 +19,11 @@ const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
-function calculaTempo(tempoObjetivo){
-    let tempoAtual = new Date ();
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    return segundos;
 }
